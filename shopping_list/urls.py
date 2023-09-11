cbb15de7c17170a6f0c4e8e59527bd91f150475a
Muhammad Hanif <muhammadhanif2404@gmail.com>
2023-09-11 11:25:47 +0700

    Fix urls.py

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.urls import path
 from django.urls import path, include
-from django.contrib import admin
+from main.views import show_main
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', show_main, name='show_main'),
     path('main/', include('main.urls')),
 ]
